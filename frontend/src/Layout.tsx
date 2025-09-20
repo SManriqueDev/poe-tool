20e2b8d0968445b06f6c7b4bd22597974b56bb7f
SManriqueDev <yhoan.manrique@correo.uis.edu.co>
2025-09-20 11:18:57 -0500

    refactor: Adjust import order and enhance Layout component structure

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,17 +1,19 @@
+import type * as React from "react";
 import { AppSidebar } from "@/components/app-sidebar";
 import { ModeToggle } from "@/components/mode-toggle";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import type * as React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarTrigger />
-      <main className="flex min-h-screen w-full flex-col pr-6">{children}</main>
-      <div className="fixed bottom-4 right-4">
-        <ModeToggle />
-      </div>
-    </SidebarProvider>
-  );
+	return (
+		<SidebarProvider>
+			<AppSidebar />
+			<SidebarTrigger />
+			<main className="relative flex min-h-svh overflow-auto w-full flex-col pr-6">
+				{children}
+			</main>
+			<div className="fixed bottom-4 right-4">
+				<ModeToggle />
+			</div>
+		</SidebarProvider>
+	);
 }
